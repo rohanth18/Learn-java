2be68a10804754575191aef305d9b14adfd58119
Rohanth S R <156525984+rohanth18@users.noreply.github.com>
2026-09-05 21:35:01 +0530

    Add files via upload

--- Octal.java
+++ Octal.java
@@ -1,34 +1,53 @@
 import java.util.*;
 public class Octal 
 {
-    public static void main(String[] args) 
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number to check number system");
+    String num = sc.next(); // 127 now this a word 
+    boolean octal = true;// assumne that the number is octal
+    boolean binary=true;
+    boolean decimal=true;
+    boolean hexadecimal=true;
+    for(int i = 0 ; i<num.length();i++)
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number");
-        String num=sc.next();//"127" -->127
-        int type=2;
-        for(int i=0;i<=num.length()-1;i++)
+        char ch = num.charAt(i);
+        //binary
+        if(ch<'0'||ch>'1')
         {
-            char ch=num.charAt(i);
-            if(ch>='8' && ch<='9' || ch>'A' && ch<'F')
-            {
-                type=16;
-                break;
-            }
-            else if(ch>='0' && ch<='7')
-            {
-                type=8;
-                break;
-            }
+            binary=false;
+         
         }
-       if(type==16)
-       {
-        System.out.println("Yes, it is hexadecimal and radix base is 16");
-       }
-       else if(type==8)
-       {
-        System.out.println("it is not octal");
-       }
+
+        if(ch<'0' || ch>'7')
+        {
+        octal = false;
+        }
+        if(ch<'0' || ch>'9')
+        {
+            decimal=false;
+        }
+        if(!((ch >= '0' && ch <= '9') ||(ch >= 'A' && ch <= 'F') ||(ch >= 'a' && ch <= 'f')))
+        {
+            hexadecimal = false;
+        }
     }
-    
+    if(binary)
+    {
+         System.out.println("this is binary");
+    }    
+    else if(octal)
+    {
+        System.out.println("this is octal and radix base is 8");
+    }
+    else if(decimal)
+    {
+         System.out.println("this is decimal");
+    }else if(hexadecimal)
+    {
+         System.out.println("this is hexadecimal");
+    }
+
+}    
 }
