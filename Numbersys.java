acf2a2a7d14dc40c3e66db57f70dd5e66f7f7cde
Rohanth S R <156525984+rohanth18@users.noreply.github.com>
2026-09-07 19:02:47 +0530

    Add files via upload

---
+++ Numbersys.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Numbersys 
+{
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number to check number system");
+    String num = sc.next(); // 127 now this a word 
+    boolean octal = true;// assumne that the number is octal
+    boolean binary=true;
+    boolean decimal=true;
+    boolean hexadecimal=true;
+    for(int i = 0 ; i<num.length();i++)
+    {
+        char ch = num.charAt(i);
+        //binary
+        if(ch<'0'||ch>'1')
+        {
+            binary=false;
+         
+        }
+
+        if(ch<'0' || ch>'7')
+        {
+        octal = false;
+        }
+        if(ch<'0' || ch>'9')
+        {
+            decimal=false;
+        }
+        if(!((ch >= '0' && ch <= '9') ||(ch >= 'A' && ch <= 'F') ||(ch >= 'a' && ch <= 'f')))
+        {
+            hexadecimal = false;
+        }
+    }
+    if(binary)
+    {
+         System.out.println("this is binary and radix base is 2");
+    }    
+    else if(octal)
+    {
+        System.out.println("this is octal and radix base is 8");
+    }
+    else if(decimal)
+    {
+         System.out.println("this is decimal and radix base is 10");
+    }else if(hexadecimal)
+    {
+         System.out.println("this is hexadecimal and radix base is 16");
+    }
+
+}    
+}
